d8458f64d31fc7a3b77d496b0ec9123ef3f06a03
amandarmarcal <amandarmarcal@gmail.com>
2020-11-21 11:10:49 -0300

    Quase pronto. Flata falha na escrita do arquivo ao dar get.

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+
 import socket
 import threading
 
@@ -8,6 +9,7 @@ destiny = (HOST, PORT)
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 conectado = True
+nome = ''
 
 def entradaDados(conectado):
     while conectado:
@@ -23,11 +25,40 @@ def entradaDados(conectado):
         elif entradaList[0] == '/list':
             udp.sendto('LIST'.encode(), destiny)
             
-        elif entradaList[0] == '/file':
-            continue
+        elif entradaList[0] == '/file':     
+            dados = open(entradaList[1], 'r')
+            tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            tcp.connect(destiny)
+            fileMsg = 'FILE:' + entradaList[1] 
+            tcp.send(fileMsg.encode())
+            tcp.recv(1024)
+            tcp.send(nome.encode())
+            tcp.recv(1024)
+            for line in dados.readlines():
+                tcp.send(line.encode())
+            tcp.close()
 
         elif entradaList[0] == '/get':
-            continue
+            tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            tcp.connect(destiny)
+            getMsg = 'GET:' + entradaList[1]
+            tcp.send(getMsg.encode())
+            tcp.recv(1024).decode()
+            ack = tcp.recv(1024).decode()
+            
+            nomeArquivo = 'client' + entradaList[1]
+            if ack != 'error':
+                arquivo = open(nomeArquivo, 'w')
+                while True:
+                    dados = tcp.recv(1024).decode()
+                    if not dados:
+                        break
+                    arquivo.write(dados)
+                tcp.close()
+            else:
+                print('Arquivo não disponível!')
+                tcp.close()
+
         else:
             info1 = 'MSG:' + entrada 
             udp.sendto(info1.encode(), destiny)
@@ -35,13 +66,24 @@ def entradaDados(conectado):
 
 def escutaServer(conectado):
     while conectado:
-        msg, host = udp.recvfrom(1024)
+        msg, host = udp.recvfrom(1024) 
+        
         if msg.decode() == 'INFO:desconectado':
             break
-        print(msg.decode())
+
+        msg = msg.decode()
+        msgList = msg.split(':')
+
+        if msgList[0] == 'INFO':
         
+            if msgList[1] == 'Clientes conectados':
+                print(msgList[1] + ':\n' + msgList[2])
+            else:
+                print(msgList[1])
 
-
+        elif msgList[0] == 'MSG':
+            print(msgList[1] + ' disse: ' + msgList[2])
+            
 
 nome = input("Nome de usuario:")
 
--- server.py
+++ server.py
@@ -1,61 +1,124 @@
 import socket
+import threading
 
-HOST = 'localhost'
+HOST = ''
 PORT = 20000
 
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 origin = (HOST, PORT)
 udp.bind(origin)
+
+tcp.bind(origin)
+tcp.listen(1)
+
 usuariosPorNome = {}
 usuariosPorEndereco = {} 
+arquivo = None
+nomeArquivo = ''
 
-while True:
-    msg, cliente = udp.recvfrom(1024)
+def conexaoUDP():
+    while True:
+        msg, cliente = udp.recvfrom(1024)
 
-    msg = msg.decode()
-    msgList = msg.split(':')
-
-
-    if msgList[0] == 'USER':
-        info1 = 'INFO:' + msgList[1] + ' entrou'
-        for user in usuariosPorNome:
-            udp.sendto(info1.encode(), usuariosPorNome[user])
-        usuariosPorNome[msgList[1]] = cliente 
-        usuariosPorEndereco[cliente] = msgList[1] 
-
-        
-    elif msgList[0] == 'MSG':
-        info2 = 'MSG:' + usuariosPorEndereco[cliente] + ':' + msgList[1]
-        for user in usuariosPorEndereco:
-            if(user != cliente):
-                udp.sendto(info2.encode(), user)
+        msg = msg.decode()
+        msgList = msg.split(':')
 
 
-    elif msgList[0] == 'LIST':  
-        s = ', '
-        listC = []
-        for user in usuariosPorNome:
-            listC.append(user)
-        listM = s.join(listC)
-        info3 = 'INFO:' + listM
-        udp.sendto(info3.encode(), cliente)
+        if msgList[0] == 'USER':
+            info1 = 'INFO:' + msgList[1] + ' entrou'
+            for user in usuariosPorNome:
+                udp.sendto(info1.encode(), usuariosPorNome[user])
+            usuariosPorNome[msgList[1]] = cliente 
+            usuariosPorEndereco[cliente] = msgList[1] 
 
-    elif msgList[0] == 'FILE':
-        print('FILE')
-
-    elif msgList[0] == 'GET':
-        print('GET')    
-
-    elif msgList[0] == 'BYE':
-        nome2 = usuariosPorEndereco.pop(cliente)
-        usuariosPorNome.pop(nome2)
-
-        info4 = 'INFO:' + nome2 + ' saiu'
-
-        udp.sendto('INFO:desconectado'.encode(), cliente)
-
-        for user in usuariosPorNome:
-            udp.sendto(info4.encode(), usuariosPorNome[user])
+            
+        elif msgList[0] == 'MSG':
+            info2 = 'MSG:' + usuariosPorEndereco[cliente] + ':' + msgList[1]
+            for user in usuariosPorEndereco:
+                if(user != cliente):
+                    udp.sendto(info2.encode(), user)
 
 
-udp.close()
+        elif msgList[0] == 'LIST':  
+            s = ', '
+            listC = []
+            for user in usuariosPorNome:
+                listC.append(user)
+            listM = s.join(listC)
+            
+            info3 = 'INFO:' + 'Clientes conectados:' + listM
+            udp.sendto(info3.encode(), cliente)
+            
+        elif msgList[0] == 'GET':
+            print('GET')    
+
+        elif msgList[0] == 'BYE':
+            nome2 = usuariosPorEndereco.pop(cliente)
+            usuariosPorNome.pop(nome2)
+
+            info4 = 'INFO:' + nome2 + ' saiu'
+
+            udp.sendto('INFO:desconectado'.encode(), cliente)
+
+            for user in usuariosPorNome:
+                udp.sendto(info4.encode(), usuariosPorNome[user])
+
+def conexaoTCP():
+    while True:
+        try:
+            con, clienteTCP = tcp.accept()       
+            msgTCP = con.recv(1024).decode()
+            con.send('ok'.encode())
+            msgListTCP = msgTCP.split(':')
+
+            if msgListTCP[0] == 'FILE':
+                nomeCliente = con.recv(1024).decode()
+                con.send('ok'.encode())
+                #nomeCliente = nomeCliente[:-1]
+                nomeArquivo = msgListTCP[1]
+
+                nomeCache = 'cache' + nomeArquivo
+                arquivo = open(nomeCache, 'w')
+                while True:
+                    dados = con.recv(1024).decode()
+                    if not dados:
+                        break
+                    arquivo.write(dados)
+                arquivo.close()
+                con.close()
+
+                info5 = 'INFO:' + nomeCliente + ' enviou ' + msgListTCP[1]
+                for user in usuariosPorNome:
+                    if(user != nomeCliente):
+                        udp.sendto(info5.encode(), usuariosPorNome[user])
+
+            elif msgListTCP[0] == 'GET':
+                con.send(''.encode())
+
+                print(msgTCP)
+                print(nomeArquivo)
+                
+                if msgListTCP[1] == nomeArquivo:
+                    con.send('ok'.encode())
+                    nomeCache = 'cache' + nomeArquivo
+                    print(nomeCache)
+                    arquivo = open(nomeCache, 'r')
+                    for line in arquivo.readlines():
+                        con.send(line.encode())
+                    con.close()
+                else:    
+                    con.send('error'.encode())
+                    con.close()
+        except Exception as ex:
+                print (ex)
+
+
+t1 = threading.Thread(target= conexaoUDP)
+t1.start()
+t2 = threading.Thread(target= conexaoTCP)
+t2.start()
+
